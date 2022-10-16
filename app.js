cc972a4cd6b14834cc81325e4d696bf5815207f4
Hagevers <amoihadad1@gmail.com>
2022-10-16 20:12:43 +0300

    fixed untracked files

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: true
 }));
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://fierce-caverns-88917.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });  
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: "World"}));
 });
