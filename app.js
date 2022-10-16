f961892e112017c13441d09a1b68c56450767214
Hagevers <amoihadad1@gmail.com>
2022-10-16 10:20:15 +0300

    added api

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: "World"}));
 });
+app.get('/try', function (req, res) {
+    res.status(200).send({msg: "Amit Hagever"});
+});
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
