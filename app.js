b64ab28b00e66d48c841e4c27811423f1df36213
Hagevers <amoihadad1@gmail.com>
2022-10-24 23:45:11 +0300

    added resources api

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ var corsOptions = {
 }
     
 app.use(cors(corsOptions));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "https://fierce-caverns-88917.herokuapp.com");
-//     //res.header("Access-Control-Allow-Origin", "http://localhost:3000");  // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });  
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "https://fierce-caverns-88917.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");  // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });  
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: "World"}));
 });
