69015922cdfb1c662bfa6c5519f8732abfd159e0
Hagevers <amoihadad1@gmail.com>
2022-10-27 23:54:50 +0300

    added resources api

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyJwt = (req,res,next) => {
     console.log('entered middle');
-    const header = req.headers['Authorization'];
+    const header = req.headers.Authorization;
     const token = header && header.split(' ')[1];
     console.log(header);
     console.log(token);
