cf90449b099be67478aa0b11a173fe79fae6918a
Hagevers <amoihadad1@gmail.com>
2022-10-27 22:55:22 +0300

    added resources api

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(function(req, res, next) {
     // res.header("Access-Control-Allow-Origin", "https://fierce-caverns-88917.herokuapp.com");
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");  // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", "true");
 
     next();
   });  
