6c399d2ebc841af1cb8a82b0dc4bbc54b024eaa8
Hagevers <amoihadad1@gmail.com>
2022-11-01 00:19:03 +0200

    Schema working

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,12 +5,11 @@ const verifyJwt = (req,res,next) => {
     const header = req.header('Authorization');
     const token = header && header.split(' ')[1];
     if (!token){ 
+        console.log(req);
         return res.render('/Amit', {title: "Login first please!"})
-        // return res.status(401).send({msg: "Login first please!"})
     }
     jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
     if(err){ 
-        window.location.href = '/'
         return res.status(403).send({msg:"Not authoraized"})
     }
       req.token = token;
