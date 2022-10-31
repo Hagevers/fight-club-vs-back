c31584ee51f3540d1eec105a4ec247726610f943
Hagevers <amoihadad1@gmail.com>
2022-10-31 23:37:32 +0200

    Schema working

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const verifyJwt = (req,res,next) => {
     const header = req.header('Authorization');
     const token = header && header.split(' ')[1];
     if (!token){ 
-        return res.render('/').status(401).send({msg: "Login first please!"})
+        return res.render('/', {msg: "Login first please!"})
         // return res.status(401).send({msg: "Login first please!"})
     }
     jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
