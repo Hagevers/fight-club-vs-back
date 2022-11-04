effe7cb12d99527ade2838dc13a4b1594e441491
Hagevers <amoihadad1@gmail.com>
2022-11-04 13:11:18 +0200

    added redirect

--- app.js
+++ app.js
@@ -145,15 +145,14 @@ app.get('/getResources', auth, async function (request, response) {
         .catch(error => console.log(error));
 });
 
-const updateRes = schedule.scheduleJob('*/1 * * * * *', function(){
-//   signUpTemplate.updateMany({},
-//     { $inc: 
-//         {'Resources.Gold': 1, 'Resources.Marble': 1,'Resources.Food': 1,'Resources.Solfour': 1}
-//     }, function(err, response){
-//         if(err) console.log(err);
-//         else console.log(response);
-//     });
-    console.log('Amit');
+const updateRes = schedule.scheduleJob('*/1 * * * *', function(){
+  signUpTemplate.updateMany({},
+    { $inc: 
+        {'Resources.Gold': 1, 'Resources.Marble': 1,'Resources.Food': 1,'Resources.Solfour': 1}
+    }, function(err, response){
+        if(err) console.log(err);
+        else console.log(response);
+    });
 });
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
