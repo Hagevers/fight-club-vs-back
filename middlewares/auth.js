55751e755268f4d6325ba9866bdaaaaf74c9f82b
Hagevers <amoihadad1@gmail.com>
2022-12-02 13:17:37 +0200

    Added api confirm

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,7 +14,8 @@ const verifyJwt = (req,res,next) => {
     .then(data =>{
         console.log(data.isVerified);
         if (data.isVerified === false){
-            return next(Error('User is not verified'))
+            res.status(403).send({msg:"User Not Verified"});
+            req.connection.destroy();
         }
     })
     jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
