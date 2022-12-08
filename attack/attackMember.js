9cc790e940fb8d538789acb03825e95e4be63c2e
Hagever <amoihadad1@gmail.com>
2022-12-08 18:26:57 +0200

    id

--- attack/attackMember.js
+++ attack/attackMember.js
@@ -1,14 +1,11 @@
 const schema = require('../models/UserTemplate');
 
 exports.attackMember = function(request, response){
-    const {attacked} = request.params;
+    const {id} = request.params;
     const {attacker} = request.body;
     
     let AttackerPower,AttackedPower;
-
-    console.log('this is the id', attacked);
-    console.log('this is all the params', request.params);
-    schema.findOne({_id: attacked})
+    schema.findOne({_id: id})
     .select('Power Resources')
     .then(data => {
         schema.findOne({_id:attacker})
