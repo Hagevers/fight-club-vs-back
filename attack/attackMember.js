8aab70f7b0a3723982ab9c310008c3dceda48bb2
Hagever <amoihadad1@gmail.com>
2022-12-08 18:05:24 +0200

    log

--- attack/attackMember.js
+++ attack/attackMember.js
@@ -8,6 +8,7 @@ exports.attackMember = function(request, response){
     .then(data => {
         const attackDet = schema.findOne({_id:attacker})
         .select('Power')
+        console.log(attackDet);
         AttackerPower = attackDet.Power.Soldiers.Ammount * attackDet.Power.Items;
         AttackedPower = data.Power.Soldiers.Ammount * data.Power.Items;
         console.log(AttackerPower + " " + AttackedPower);
