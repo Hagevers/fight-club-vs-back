6393b98616d7ad08d27b9458c7782426bf378c05
Hagever <amoihadad1@gmail.com>
2022-12-08 21:20:45 +0200

    foreach

--- attack/attackMember.js
+++ attack/attackMember.js
@@ -4,16 +4,19 @@ exports.attackMember = function(request, response){
     const {id} = request.params;
     const {attacker} = request.body;
     
-    let AttackerPower,AttackedPower;
+    let attackerPower,attackedPower;
     schema.findOne({_id: id})
     .select('Power Resources')
     .then(data => {
         schema.findOne({_id:attacker})
         .select('Power')
         .then(attackDet => {
-            AttackerPower = attackDet.Power.Soldiers.Ammount * attackDet.Power.Items[0].power;
-            console.log(data);
-            AttackedPower = data.Power.Soldiers.Ammount * data.Power.Items[0].power;
+            attackDet.Power.Items.forEach(item => attackerPower += item);
+            attackerPower *= attackDet.Power.Soldiers.Ammount;
+            
+            data.Power.Items.forEach(item => attackedPower += item);
+            attackedPower *= data.Power.Soldiers.Ammount;
+            
             console.log(AttackerPower + " " + AttackedPower);
         });
         
