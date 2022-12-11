fa543f717c7fb97924f162c65d7112019d26fbf7
Hagevers <amoihadad1@gmail.com>
2022-12-11 20:52:31 +0200

    asd

--- attack/attackMember.js
+++ attack/attackMember.js
@@ -3,13 +3,13 @@ const schema = require('../models/UserTemplate');
 exports.attackMember = function(request, response){
     const {id} = request.params;
     const {attacker} = request.body;
-    
+    let resources = {};
     let attackerPower = 0,attackedPower = 0;
     schema.findOne({_id: id})
     .select('Power Resources')
     .then(data => {
         schema.findOne({_id:attacker})
-        .select('Power Resources')
+        .select('Power Resources NickName')
         .then(attackDet => {
 
             attackDet.Power.Items.map(item => attackerPower += item.power);
@@ -19,10 +19,12 @@ exports.attackMember = function(request, response){
             attackedPower *= data.Power.Soldiers.Ammount;
 
             if(attackerPower > attackedPower){
-                attackDet.$inc('Resources.Gold', (data.Resources.Gold * 0.3)).save();
                 data.$inc('Resources.Gold', -(data.Resources.Gold * 0.3)).save();
+                resources['Gold'] = attackDet.$inc('Resources.Gold', (data.Resources.Gold * 0.3)).save();
+                resources['NickName'] = attackDet.NickName;
+                resources['Alliance'] = attackDet.alliance;
             }
-
+            response.status(200).send(resources)
         });
         
        
