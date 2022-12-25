8ffd3cf23947ea0a3a701fb9e16261ded91e8cc5
Hagevers <amoihadad1@gmail.com>
2022-12-25 23:40:07 +0200

    push

--- attack/attackMember.js
+++ attack/attackMember.js
@@ -79,8 +79,8 @@ exports.buyItem = function(request, response){
     schema.findOneAndUpdate({_id: id}).then(data => {
         console.log(data.Power.Items);
         console.log(item);
+        data.Power.Items.push({name: item.name, power: item.power})
         return response.status(200)
-        // data.Power.Items.push(item)
     })
     
 }
